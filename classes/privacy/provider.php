d158cd29fba029b551376fc9b8403323e243b84d
gthomas2 <brudinie@gmail.com>
2018-06-28 11:21:16 +0100

    Issue #9: Add privacy provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -15,15 +15,25 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Image Optimiser
- * @author    Guy Thomas <brudinie@gmail.com>
- * @copyright Copyright (c) 2016 Guy Thomas.
+ * Privacy provider settings
+ *
+ * @package   filter_imageopt
+ * @copyright Copyright (c) 2018 Guy Thomas
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018062800;
-$plugin->requires  = 2011111500;
-$plugin->component = 'filter_imageopt';
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '3.5.0.0';
+namespace filter_imageopt\privacy;
+
+// This plugin does not store any personal user data.
+class provider implements \core_privacy\local\metadata\null_provider {
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}
